f82b1957f8130fb89852acebab036b5a3f70c6e3
ping7985 <50404777+ping7985@users.noreply.github.com>
2019-12-06 01:41:19 +0900

    멜론과 동일

--- genie.py
+++ genie.py
@@ -1,6 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
 
+temp_gtlist = []
+temp_galist = []
+
 headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
 url = 'https://www.genie.co.kr/chart/top200'
 resp = requests.get(url, headers = headers)
@@ -11,9 +14,11 @@ songs = soup.select('#body-content > div.newest-list > div > table > tbody > tr'
 for song in songs:
     title = song.find('td',{'class':'info'}).find('a',{'class':'title ellipsis'}).text
     artist = song.find('td',{'class':'info'}).find('a',{'class':'artist ellipsis'}).text
-    print(title.strip() + " - " +artist)
+    'print(title.strip() + " - " +artist)'
+    temp_gtlist.append(title.strip())
+    temp_galist.append(artist)
 
-    
+
 headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
 url = 'https://www.genie.co.kr/chart/top200?ditc=D&pg=2'
 resp = requests.get(url, headers = headers)
@@ -24,4 +29,6 @@ songs = soup.select('#body-content > div.newest-list > div > table > tbody > tr'
 for song in songs:
     title = song.find('td',{'class':'info'}).find('a',{'class':'title ellipsis'}).text
     artist = song.find('td',{'class':'info'}).find('a',{'class':'artist ellipsis'}).text
-    print(title.strip() + " - " +artist)
+    'print(title.strip() + " - " +artist)'
+    temp_gtlist.append(title.strip())
+    temp_galist.append(artist)
