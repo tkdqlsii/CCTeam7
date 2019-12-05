e6fc0cd73b6ce0961a7d5870b52895a626a64ead
tkdqlsii <43057269+tkdqlsii@users.noreply.github.com>
2019-12-05 20:12:04 +0900

    genie complete
    지니 1-100위까지 정보를 받아왔다

--- genie.py
+++ genie.py
@@ -1,15 +1,27 @@
-import re
 import requests
 from bs4 import BeautifulSoup
 
-headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chome/73.0.3683.86 safari/537.36'}
-genie = requests.get('https://www.genie.co.kr/chart/top200/', headers = headers)
-genie_html = genie.text
-genie_parse = BeautifulSoup(genie_html, 'html.parser')
+headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+url = 'https://www.genie.co.kr/chart/top200'
+resp = requests.get(url, headers = headers)
+soup = BeautifulSoup(resp.text, 'html.parser')
 
-title = genie_parse.select('#body-content > div.newest-list > div > table > tbody > tr:nth-child(1) > td.info > a.title.ellipsis')
-artist = genie_parse.select('#body-content > div.newest-list > div > table > tbody > tr:nth-child(1) > td.info > a.artist.ellipsis')
-songs = genie_parse.find('td',{'class':'info'}).find('a',{'class':'title ellipsis'}).text
+songs = soup.select('#body-content > div.newest-list > div > table > tbody > tr')
 
-for song in title:
-    print(title.text.strip() + " - " + artist.text)
+for song in songs:
+    title = song.find('td',{'class':'info'}).find('a',{'class':'title ellipsis'}).text
+    artist = song.find('td',{'class':'info'}).find('a',{'class':'artist ellipsis'}).text
+    print(title.strip() + " - " +artist)
+
+    
+headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+url = 'https://www.genie.co.kr/chart/top200?ditc=D&ymd=20191205&hh=20&rtm=Y&pg=2'
+resp = requests.get(url, headers = headers)
+soup = BeautifulSoup(resp.text, 'html.parser')
+
+songs = soup.select('#body-content > div.newest-list > div > table > tbody > tr')
+
+for song in songs:
+    title = song.find('td',{'class':'info'}).find('a',{'class':'title ellipsis'}).text
+    artist = song.find('td',{'class':'info'}).find('a',{'class':'artist ellipsis'}).text
+    print(title.strip() + " - " +artist)
