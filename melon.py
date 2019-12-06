3ae7bcc156e90464733da5f6252e698feeaea183
cwelly <potato3884@naver.com>
2019-12-07 00:24:12 +0900

    update db line
    mysql 테이블에 크롤링한 데이터를 추가한다

--- melon.py
+++ melon.py
@@ -2,15 +2,13 @@ import pymysql
 import requests
 
 from bs4 import BeautifulSoup
+pw='여기다 DB 비번을 써주세요'
 
-conn = pymysql.connect(host='localhost', user='root', password='gusgh4528',
+conn = pymysql.connect(host='localhost', user='root', password=pw,
                        db='testdb', charset='utf8')
 curs=conn.cursor()
-#sql="select * from testdb.users"
-sql=""
-#curs.execute(sql)
-#rows=curs.fetchall()
-#print(rows)
+sql="truncate table genie"#데이터베이스 초기화 시킨다.
+curs.execute(sql)
 
 temp_tlist = []
 temp_alist = []
@@ -24,10 +22,9 @@ title = melon_parse.select('#lst50 > td:nth-child(6) > div > div > div.ellipsis.
 artist = melon_parse.select('#lst50 > td:nth-child(6) > div > div > div.ellipsis.rank02 > span > a:nth-child(1)')
 
 for r in range(50):
-    'print(title[r].text + " - " + artist[r].text)'
+    print(title[r].text + " - " + artist[r].text)
     temp_tlist.append(title[r].text)
     temp_alist.append(artist[r].text)
-
 
 header = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; Trident/7.0; rv:11.0) like Gecko'}
 melon = requests.get('https://www.melon.com/chart/index.htm#params%5Bidx%5D=51', headers = header) # 멜론차트 웹사이트
@@ -35,16 +32,23 @@ melon_html = melon.text
 melon_parse= BeautifulSoup(melon_html, 'html.parser')
 title = melon_parse.select('#lst100 > td:nth-child(6) > div > div > div.ellipsis.rank01 > span > a')
 artist = melon_parse.select('#lst100 > td:nth-child(6) > div > div > div.ellipsis.rank02 > span > a:nth-child(1)')
-sql="""insert into customer(title, artist, score)
-values ($s , $s ,$d)"""
+sql="""
+INSERT INTO genie (title, artist, score)
+VALUES (%s, %s, %s)
+"""
 for r1 in range(50):
     print(title[r1].text + " - " + artist[r1].text)
     temp_tlist.append(title[r1].text)
     temp_alist.append(artist[r1].text)
 for r in range(100):
-    curs.execute(sql,(temp_tlist[r],temp_alist[r], r) )
-    print(r)
-    
+    rc=r+1
+    curs.execute(sql,(temp_tlist[r],temp_alist[r], rc) )
+#데이터베이스 조회해서 확인하는 과정
+sql="select * from testdb.genie"
+curs.execute(sql)
+rows=curs.fetchall()
+print(rows)
+
     
 
 conn.commit()
