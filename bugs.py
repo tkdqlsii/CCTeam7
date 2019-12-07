06713ee65e33bcc50648df850058226cc99e7fa4
cwelly <potato3884@naver.com>
2019-12-07 22:52:06 +0900

    update bugs.py -> add img att
    이미지를 추가로 크롤링 해오고, 그것을 db에 넣는 부분을 새로 추가했다

--- bugs.py
+++ bugs.py
@@ -3,8 +3,8 @@ import new
 import pymysql
 
 from bs4 import BeautifulSoup
-
-pw='당신의 db의 비밀번호를 넣어주세요'
+#pw='당신의 비밀번호를 입력해주세요'
+pw='gusgh4528'
 new.dbcheck()  #테이블이 있는지 확인하는 모듈의 함수
 conn = pymysql.connect(host='localhost', user='root', password=pw,
                        db='testdb', charset='utf8')
@@ -20,6 +20,7 @@ bugs_parse= BeautifulSoup(bugs_html, 'html.parser')
 
 title_arr=[]
 artist_arr=[]
+img_arr=[]
 
 # sep은 분리할 단어 replace로 모든 feat에 해당되는  대소문자 구분없이 feat라는 단어로 변경 후 분리
 for link1 in bugs_parse.find_all(name="p",attrs={"class":"title"}):
@@ -30,16 +31,23 @@ for link1 in bugs_parse.find_all(name="p",attrs={"class":"title"}):
 for link1 in bugs_parse.find_all(name="p",attrs={"class":"artist"}):
     artist_arr.append(link1.find('a').text)
 
+#이미지를 크롤링 해오는 파트
+for link1 in bugs_parse.find_all(name="a",attrs={"class":"thumbnail"}):
+    img_arr.append(link1.find('img').get("src"))
+    
+
 #출력부분 주석처리
 """for s in range(100):
     print(str(s)+"위")
     print("제목: " + title_arr[s] + "/ 가수 :"+artist_arr[s])"""
 
-sql="""INSERT INTO bugs (title, artist, score) VALUES (%s, %s, %s)"""
+sql="""INSERT INTO bugs (title, artist, score, img) VALUES (%s, %s, %s, %s)"""
 
 for r in range(100):
     rc=r+1
-    curs.execute(sql,(title_arr[r],artist_arr[r], rc) )
+    curs.execute(sql,(title_arr[r],artist_arr[r], rc, img_arr[r]) )
 
 conn.commit()
 conn.close()
+
+
