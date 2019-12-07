037f5263593add7c55e3531190c974c0e554ed31
cwelly <potato3884@naver.com>
2019-12-07 22:48:41 +0900

    update melon.py -> add "img" att
    이미지를 사용할것이기 때문에, 멜론에서 이미지링크를 추가로 크롤링 해오게 만들었다

--- melon.py
+++ melon.py
@@ -3,7 +3,7 @@ import requests
 import new
 
 from bs4 import BeautifulSoup
-pw='당신의 db의 비밀번호를 넣어주세요'
+pw='gusgh4528'
 new.dbcheck() #테이블이 있는지 확인하는 모듈의 함수
 
 conn = pymysql.connect(host='localhost', user='root', password=pw,
@@ -15,6 +15,7 @@ curs.execute(sql)
 
 temp_tlist = []
 temp_alist = []
+temp_ilist=[]
 
 header = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; Trident/7.0; rv:11.0) like Gecko'}
 melon = requests.get('https://www.melon.com/chart/index.htm', headers = header) # 멜론차트 웹사이트
@@ -23,12 +24,14 @@ melon_parse= BeautifulSoup(melon_html, 'html.parser')
 
 title = melon_parse.select('#lst50 > td:nth-child(6) > div > div > div.ellipsis.rank01 > span > a')
 artist = melon_parse.select('#lst50 > td:nth-child(6) > div > div > div.ellipsis.rank02 > span > a:nth-child(1)')
-
+img=melon_parse.select('#lst50 > td:nth-child(4) > div > a > img')
 for r in range(50):
     sep = 'feat'
-    tmp = title[r].text.replace('(Feat',sep).replace('Feat',sep).replace('(feat',sep).split(sep)[0]
+    tmp = title[r].text.replace('(Feat',sep).replace('feat',sep).replace('Feat',sep).replace('(feat',sep).split(sep)[0]
+    img_src=img[r].get("src")
     temp_tlist.append(tmp)
     temp_alist.append(artist[r].text)
+    temp_ilist.append(img_src)
 
 header = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; Trident/7.0; rv:11.0) like Gecko'}
 melon = requests.get('https://www.melon.com/chart/index.htm#params%5Bidx%5D=51', headers = header) # 멜론차트 웹사이트
@@ -36,24 +39,25 @@ melon_html = melon.text
 melon_parse= BeautifulSoup(melon_html, 'html.parser')
 title = melon_parse.select('#lst100 > td:nth-child(6) > div > div > div.ellipsis.rank01 > span > a')
 artist = melon_parse.select('#lst100 > td:nth-child(6) > div > div > div.ellipsis.rank02 > span > a:nth-child(1)')
-
+img=melon_parse.select('#lst50 > td:nth-child(4) > div > a > img')
 for r1 in range(50):
     sep = 'feat'
-    tmp = title[r1].text.replace('(Feat',sep).replace('Feat',sep).replace('(feat',sep).split(sep)[0]
+    tmp = title[r1].text.replace('(Feat',sep).replace('feat',sep).replace('Feat',sep).replace('(feat',sep).split(sep)[0]
+    img_src=img[r].get("src")
     temp_tlist.append(tmp)
     temp_alist.append(artist[r1].text)
+    temp_ilist.append(img_src)
 
-sql="""INSERT INTO melon (title, artist, score) VALUES (%s, %s, %s)"""
+sql="""INSERT INTO melon (title, artist, score,img) VALUES (%s, %s, %s ,%s)"""
 
 for r in range(100):
     rc=r+1
-    curs.execute(sql,(temp_tlist[r],temp_alist[r], rc) )
+    curs.execute(sql,(temp_tlist[r],temp_alist[r], rc,temp_ilist[r]) )
 
 #데이터베이스 조회해서 확인하는 과정
-"""sql="select * from testdb.melon"
+sql="select * from testdb.melon"
 curs.execute(sql)
 rows=curs.fetchall()
-print(row)"""
 
 conn.commit()
 conn.close()
