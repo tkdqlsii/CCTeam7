13f136dd71124e7c90e6372b716c9ff5d5ee49e2
ping7985 <50404777+ping7985@users.noreply.github.com>
2019-12-07 21:32:01 +0900

    Update genie.py

--- genie.py
+++ genie.py
@@ -9,7 +9,6 @@ conn = pymysql.connect(host='localhost', user='root', password=pw,
                        db='testdb', charset='utf8')
 curs=conn.cursor()
 
-
 sql="truncate table genie"#데이터베이스 초기화 시킨다.
 curs.execute(sql)
 
@@ -24,12 +23,12 @@ soup = BeautifulSoup(resp.text, 'html.parser')
 songs = soup.select('#body-content > div.newest-list > div > table > tbody > tr')
 
 for song in songs:
+    sep = 'feat'
     title = song.find('td',{'class':'info'}).find('a',{'class':'title ellipsis'}).text
     artist = song.find('td',{'class':'info'}).find('a',{'class':'artist ellipsis'}).text
-    print(title.strip() + " - " +artist)
-    temp_gtlist.append(title.strip())
+    tmp = title.replace('(feat',sep).replace('(Feat',sep).replace('Feat',sep).split(sep)[0]
+    temp_gtlist.append(tmp.strip())
     temp_galist.append(artist)
-
 
 headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
 url = 'https://www.genie.co.kr/chart/top200?ditc=D&pg=2'
@@ -39,22 +38,17 @@ soup = BeautifulSoup(resp.text, 'html.parser')
 songs = soup.select('#body-content > div.newest-list > div > table > tbody > tr')
 
 for song in songs:
+    sep = 'feat'
     title = song.find('td',{'class':'info'}).find('a',{'class':'title ellipsis'}).text
     artist = song.find('td',{'class':'info'}).find('a',{'class':'artist ellipsis'}).text
-    print(title.strip() + " - " +artist)
-    temp_gtlist.append(title.strip())
+    tmp = title.replace('(feat',sep).replace('(Feat',sep).replace('Feat',sep).split(sep)[0]
+    temp_gtlist.append(tmp.strip())
     temp_galist.append(artist)
 
-sql="""
-INSERT INTO genie (title, artist, score)
-VALUES (%s, %s, %s)
-"""
+sql="""INSERT INTO genie (title, artist, score) VALUES (%s, %s, %s)"""
 for r in range(100):
     rc=r+1
     curs.execute(sql,(temp_gtlist[r],temp_galist[r], rc) )
 
- 
-
 conn.commit()
 conn.close()
-
