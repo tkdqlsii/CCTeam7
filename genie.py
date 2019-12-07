b30bccc20af47bcdc06be819dcd4ea87c51b574c
cwelly <potato3884@naver.com>
2019-12-07 22:47:24 +0900

    update genie.py ->add "img" att
    이미지 링크를 사용할것이기 때문에, 기존에 것에서 추가로 이미지 링크또한 크롤링 해올수 있도록 수정했다

--- genie.py
+++ genie.py
@@ -3,7 +3,7 @@ import pymysql
 import new #table이 있는지 확인하는 모듈
 from bs4 import BeautifulSoup
 
-pw='당신의 db의 비밀번호를 넣어주세요'
+pw='gusgh4528'
 new.dbcheck()  #테이블이 있는지 확인하는 모듈의 함수
 conn = pymysql.connect(host='localhost', user='root', password=pw,
                        db='testdb', charset='utf8')
@@ -14,7 +14,7 @@ curs.execute(sql)
 
 temp_gtlist = []
 temp_galist = []
-
+temp_gilist=[]
 headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
 url = 'https://www.genie.co.kr/chart/top200'
 resp = requests.get(url, headers = headers)
@@ -26,9 +26,11 @@ for song in songs:
     sep = 'feat'
     title = song.find('td',{'class':'info'}).find('a',{'class':'title ellipsis'}).text
     artist = song.find('td',{'class':'info'}).find('a',{'class':'artist ellipsis'}).text
+    img_url="http:"+song.find('img').get("src")
     tmp = title.replace('(feat',sep).replace('(Feat',sep).replace('Feat',sep).split(sep)[0]
     temp_gtlist.append(tmp.strip())
     temp_galist.append(artist)
+    temp_gilist.append(img_url)
 
 headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
 url = 'https://www.genie.co.kr/chart/top200?ditc=D&pg=2'
@@ -41,14 +43,17 @@ for song in songs:
     sep = 'feat'
     title = song.find('td',{'class':'info'}).find('a',{'class':'title ellipsis'}).text
     artist = song.find('td',{'class':'info'}).find('a',{'class':'artist ellipsis'}).text
+    img_url="http:"+song.find('img').get("src")
     tmp = title.replace('(feat',sep).replace('(Feat',sep).replace('Feat',sep).split(sep)[0]
     temp_gtlist.append(tmp.strip())
     temp_galist.append(artist)
+    temp_gilist.append(img_url)
 
-sql="""INSERT INTO genie (title, artist, score) VALUES (%s, %s, %s)"""
+sql="""INSERT INTO genie (title, artist, score,img) VALUES (%s, %s, %s,%s)"""
 for r in range(100):
     rc=r+1
-    curs.execute(sql,(temp_gtlist[r],temp_galist[r], rc) )
+    curs.execute(sql,(temp_gtlist[r],temp_galist[r], rc,temp_gilist[r]) )
 
 conn.commit()
 conn.close()
+
